96732a74da7538190752f9bc068965946b447690
Ilia Sarantsev <ilya.sarantsev@gmail.com>
2021-03-14 18:51:18 +0300

    Chart responsive container

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -3,6 +3,7 @@ import {
   BarChart,
   CartesianGrid,
   ReferenceLine,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -13,14 +14,12 @@ function Graph(props: { data: Coin[] }) {
   const { data } = props
 
   return (
-    <div className="bar-chart-wrapper">
-      <BarChart
-        width={800}
-        height={250}
-        barGap={2}
-        barSize={800 / data.length}
-        data={data}
-      >
+    <ResponsiveContainer
+      className="bar-chart-wrapper"
+      width="100%"
+      height={250}
+    >
+      <BarChart barGap={2} barSize={800 / data.length} data={data}>
         <XAxis dataKey="name" />
         <YAxis tickCount={7} />
         <Tooltip />
@@ -28,7 +27,7 @@ function Graph(props: { data: Coin[] }) {
         <Bar dataKey={price} fill="#ff7300" radius={[5, 5, 5, 5]} />
         <ReferenceLine type="horizontal" y={0} stroke="#666" />
       </BarChart>
-    </div>
+    </ResponsiveContainer>
   )
 }
 
